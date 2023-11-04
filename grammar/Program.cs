0fe2d6e87022e65b465f5eebd49e2f24a4d51f62
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-11-04 10:50:46 +0800

    Add files via upload

---
+++ grammar/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;//引用程序集
+
+
+namespace HelloWorld//命名空间
+{
+    class Program//类
+    {
+        static void Main(string[] args)//主函数
+        {
+            Console.WriteLine("Hello World!");//输出 Hello World ；调用类库，Console等于类，一本书，WriteLine相当于书的章节，类里面的方法 
+            Console.ReadLine();//
+        }
+    }
+}
