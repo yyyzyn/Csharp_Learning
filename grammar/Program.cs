3fa95befb978676c0dadb7a07c6201c1890f0e22
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-11-04 10:51:19 +0800

    Add files via upload

--- grammar/Program.cs
+++ grammar/Program.cs
@@ -1,17 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-using System;//引用程序集
+using System;
 
-
-namespace HelloWorld//命名空间
+namespace Decisions
 {
-    class Program//类
+    class Program
     {
-        static void Main(string[] args)//主函数
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");//输出 Hello World ；调用类库，Console等于类，一本书，WriteLine相当于书的章节，类里面的方法 
-            Console.ReadLine();//
+            Console.WriteLine("Bob's Big GiveWay");
+            Console.Write("Choose a door:1,2 or 3");
+
+            /*
+            string userValue = Console.ReadLine();
+            
+            if(userValue=="1")//if判断语句，"=="判断真伪  
+            {
+                string message = "You win a new switch";
+                Console.WriteLine(message);
+            }
+            else if(userValue=="2")//else if 判断语句
+            {
+                string message = "You win a new XBOX";
+                Console.WriteLine(message);
+            }
+            else if(userValue=="3")
+            {
+                string message = "You win a new PS5";
+                Console.WriteLine(message);
+            }
+
+            else
+            {
+                string message = "Sorry";
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+            */
+
+            //第二种写法
+            string userValue = Console.ReadLine();
+            string message = "";
+
+
+            if (userValue == "1")//if判断语句，"=="判断真伪  
+            {
+                message = "You win a new switch";
+            }
+            else if (userValue == "2")//else if 判断语句
+            {
+                 message = "You win a new XBOX";
+            }
+            else if (userValue == "3")
+            {
+                 message = "You win a new PS5";
+            }
+
+            else
+            {
+                 message = "Sorry";
+                //message = message + "You loss.";
+                 message += "You Lose.";//等于message = message + "You loss.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
+
+
+//列表中第一个元素永远是0，第二个元素才是1
