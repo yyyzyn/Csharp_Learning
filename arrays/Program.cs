e3b6e50702eab1c635ba0569e2be9620c5f57cdd
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-11-14 20:59:09 +0800

    Add files via upload

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -2,28 +2,59 @@
 //Console.WriteLine("Hello, World!");
 
 using System;
+using System.Reflection.Metadata;
 
-namespace arrays
+namespace understandingarrays
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] strings)
+        /*
+    {
+        //数组的学习与使用
+        int[] numbers = new int[5];//方括号在基类型后，不在变量名后面，与C/C++不一样
+        numbers[0] = 4;
+        numbers[1] = 8;
+        numbers[2] = 15;
+        numbers[3] = 16;
+        numbers[4] = 23;
+
+        Console.WriteLine(numbers[2]);
+        Console.WriteLine(numbers.Length);//输出数组的长度
+        Console.ReadLine();
+    }
+        */
+
+
+        //int[] numbers = new int[] { 4,8,15,16,23,42 };//另一种表达方式
+
+        //字符串数组使用
+        //第一种输出方法
         {
-            //字符串学习2
-            //字符串反转
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "EDA" };//定义字符串数组
 
-            string zig = "you can get your life that you want to become a person"+"if you will become strong";
-
-            char[] charArray = zig.ToCharArray();//帮助器
-            Array.Reverse(charArray);
-
-            foreach(char zigChar in charArray )
+            for (int i = 0; i < names.Length; i++)
             {
-                Console.Write(zigChar);
+                Console.WriteLine(names[i]);
+
             }
             Console.ReadLine();
 
+            string[] numbers = new string[] { "zhang", "sha", "wang", "bei" };//定义字符串数组2
+
+            //第二种输出办法
+
+            foreach(string number in numbers)
+            {
+                Console.WriteLine(number);//与python里面方法很相似
+            }
+
+            Console.ReadLine();
 
         }
+
     }
-}
+
+ }
+
+
