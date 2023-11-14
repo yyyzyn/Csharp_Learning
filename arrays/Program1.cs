a704f1224616aa22dc75fb31ee234015a525d128
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-11-14 21:03:32 +0800

    Add files via upload

---
+++ arrays/Program1.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace arrays
+{
+    class Program1
+    {
+        static void Main()
+        {
+            //字符串学习2
+            //字符串反转
+
+            string zig = "you can get your life that you want to become a person"+"if you will become strong";
+
+            char[] charArray = zig.ToCharArray();//帮助器
+            Array.Reverse(charArray);
+
+            foreach(char zigChar in charArray )
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
