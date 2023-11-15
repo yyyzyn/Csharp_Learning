c52b419d8487d7c7e130cdca0c6fdbf6b62fb022
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-11-15 20:18:46 +0800

    Add files via upload

---
+++ 方法使用/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace SimpleMethod
+{
+    class Program1
+    {
+        static void Main()
+        {
+            HelloWorld();//调用方法
+            Console.ReadLine();
+        }
+
+
+        private static void HelloWorld()//定义方法
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
